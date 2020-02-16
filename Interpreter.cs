5013f5496809681ace7a3800c43eb491ded12c9d
dltttl <52045812+dltttl@users.noreply.github.com>
2020-02-16 17:53:02 +0300

    Add files via upload

---
+++ Interpreter.cs
@@ -0,0 +1,18 @@
+using System.IO;
+
+namespace KizhiPart1
+{
+    public class Interpreter
+    {
+        private TextWriter _writer;
+
+        public Interpreter(TextWriter writer)
+        {
+            _writer = writer;
+        }
+
+        public void ExecuteLine(string command)
+        {
+        }
+    }
+}
