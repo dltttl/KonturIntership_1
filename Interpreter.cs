fc936f0c3e8433c4b43444f19ef3ed117b0b4027
dltttl <dltttl@yandex.ru>
2020-02-26 17:29:08 +0300

    fix Print Method

--- Interpreter.cs
+++ Interpreter.cs
@@ -9,9 +9,9 @@ namespace KizhiPart1
     {
         private readonly TextWriter _writer;
 
-        private  readonly Dictionary<string, Action<string[]>> _commands;
+        private readonly Dictionary<string, Action<string[]>> _commands;
 
-        private  readonly Dictionary<string, int> _variables;
+        private readonly Dictionary<string, int> _variables;
 
         private const string ErrorMessage = "Переменная отсутствует в памяти";
 
@@ -24,7 +24,7 @@ namespace KizhiPart1
                 ["set"] = (parameters) => Set(parameters[0], int.Parse(parameters[1])),
                 ["sub"] = (parameters) => Sub(parameters[0], int.Parse(parameters[1])),
                 ["print"] = (parameters) => Print(parameters[0]),
-                ["rem"] = (parameters)=>Remove(parameters[0])
+                ["rem"] = (parameters)=> Remove(parameters[0])
             };
         }
 
@@ -56,13 +56,7 @@ namespace KizhiPart1
 
         private void Print(string variableName)
         {
-            if (!_variables.ContainsKey(variableName))
-            {
-                _writer.WriteLine(ErrorMessage);
-                return;
-            }
-
-            _writer.WriteLine(_variables[variableName]);
+            _writer.WriteLine(_variables.TryGetValue(variableName, out var value) ? value.ToString() : ErrorMessage);
         }
 
         private void Remove(string variableName)
